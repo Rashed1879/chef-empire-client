d55f29d0d33fe145d74d40d4855d3c564153c60e
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-05-02 05:18:08 +0600

    Basic Setup Done

---
+++ src/layout/Main.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Home from '../pages/Home/Home';
+import Navbar from '../pages/shared/Navbar';
+import Footer from '../pages/shared/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+	return (
+		<div>
+			<Navbar></Navbar>
+			<div className="min-h-[calc(100vh-68px)]">
+				<Outlet></Outlet>
+			</div>
+			<Footer></Footer>
+		</div>
+	);
+};
+
+export default Main;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from '../layout/Main';
+import Home from '../pages/Home/Home';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Main></Main>,
+		children: [
+			{
+				path: '/',
+				element: <Home></Home>,
+			},
+		],
+	},
+]);
+
+export default router;
