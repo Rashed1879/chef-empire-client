16ea5197c5e2816bf2ff0fdcff664179e1485a4c
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-05-02 16:59:47 +0600

    Navbar Added

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,11 +1,50 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
 	return (
-		<div>
-			<h1>This is Navbar</h1>
-		</div>
+		<nav className="bg-accent text-primary font-lora">
+			<div className="flex justify-between  items-center md:p-6 p-4">
+				<h2 className="text-2xl font-bold">ChefEmpire</h2>
+				<div onClick={() => setOpen(!open)} className="md:hidden ">
+					<span>
+						{open === true ? (
+							<XMarkIcon className="h-6 w-6" />
+						) : (
+							<Bars3Icon className="h-6 w-6" />
+						)}
+					</span>
+				</div>
+				<ul
+					className={`md:flex md:space-x-5 text-primary absolute duration-500 md:static ${
+						open
+							? 'top-2 right-12 text-center bg-accent shadow-md p-2 rounded-md'
+							: '-top-96 right-12 text-center bg-accent rounded-md'
+					}`}
+				>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/blogs">Blogs</NavLink>
+					</li>
+
+					<label className="btn btn-ghost btn-circle avatar md:hidden">
+						<div className="w-10 rounded-full">
+							<img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" />
+						</div>
+					</label>
+				</ul>
+				<label className="btn btn-ghost btn-circle avatar hidden md:block">
+					<div className="w-10 rounded-full">
+						<img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" />
+					</div>
+				</label>
+			</div>
+		</nav>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,32 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		lora: ['Lora', 'serif'],
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#392137',
+
+					secondary: '#ed614a',
+
+					accent: '#faebd580',
+
+					neutral: '#191D1F',
+
+					'base-100': '#F3F3F7',
+
+					info: '#778AE9',
+
+					success: '#14A95C',
+
+					warning: '#F0B80F',
+
+					error: '#F92F3D',
+				},
+			},
+		],
 	},
 	plugins: [require('daisyui')],
 };
